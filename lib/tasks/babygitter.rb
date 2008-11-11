85c4273d6c07d3f44973d560d2e537760fae8998
Mike Schwab <mike.schwab@gmail.com>
2008-11-11 18:07:39 -0500

    added license

--- lib/tasks/babygitter.rb
+++ lib/tasks/babygitter.rb
@@ -1,5 +1,6 @@
 require 'lib/babygitter'
 namespace :babygitter do
+  desc 'reports repo metadata to public/babygitter_report.html'
   task :report do
     Babygitter::ReportGenerator.new.write_report
   end
